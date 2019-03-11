ea43fc29b8d461995e7d5305789dd0a21a5d71ea
ENDO Yasuyuki <eyasuyuki@gmail.com>
2019-03-11 14:09:10 +0900

    add _youtube_api.dart

---
+++ lib/_youtube_api.dart
@@ -0,0 +1,18 @@
+class YouTubeApi {
+  static const baseUri = 'www.googleapis.com';
+  static const youTubeEndpoint = 'youtube/v3/search';
+
+  Uri searcUri(String word, String key) {
+    return Uri.https(baseUri, youTubeEndpoint, getOptions(word, key));
+  }
+
+  Map<String, String> getOptions(String word, String key) {
+    Map<String, String> result = {
+      'part': 'snippet',
+      'maxResults': '10',
+      'key': key,
+    };
+    if (word != null) result['q'] = word;
+    return result;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/widgets.dart';
-import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'l10n/messages_all.dart';
-import 'l10n/messages_messages.dart';
+import 'l10n/app_localizations.dart';
+
+import '_youtube_api.dart';
 
 void main() => runApp(MyApp());
-
-class Strings {
-  static Future<Strings> load(Locale locale) {
-    final String name = locale.countryCode.isEmpty ? locale.languageCode : locale.toString();
-    final String localeName = Intl.canonicalizedLocale(name);
-    return initializeMessages(localeName).then((_) {
-      Intl.defaultLocale = localeName;
-      return new Strings();
-    });
-  }
-
-  static Strings of(BuildContext context) {
-    return Localizations.of<Strings>(context, Strings);
-  }
-
-  static final Strings instance = new Strings();
-
-  static final MessageLookup defaultLookup = new MessageLookup();
-  static final defaultTitle = defaultLookup.messages['title']();
-  static final defaultApiKey = defaultLookup.messages['apiKey']();
-
-  String get title => Intl.message(defaultTitle, name: "title");
-  String get apiKey => Intl.message(defaultApiKey, name: "apiKey");
-}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -43,9 +20,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      routes: <String, WidgetBuilder> {
-        '/': (BuildContext context) => new Splash(),
-        '/list': (BuildContext context) => new VideoList(),
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => new VideoList(),
         '/player': (BuildContext context) => new VideoPlayer(),
       },
       localizationsDelegates: [
@@ -65,7 +41,7 @@ class _MyLocalizationsDelegate extends LocalizationsDelegate<Strings> {
   const _MyLocalizationsDelegate();
 
   @override
-  bool isSupported(Locale locale) => ['en','ja'].contains(locale.languageCode);
+  bool isSupported(Locale locale) => ['en', 'ja'].contains(locale.languageCode);
 
   @override
   Future<Strings> load(Locale locale) => Strings.load(locale);
@@ -74,62 +50,49 @@ class _MyLocalizationsDelegate extends LocalizationsDelegate<Strings> {
   bool shouldReload(_MyLocalizationsDelegate old) => false;
 }
 
-class Splash extends StatefulWidget {
-  @override
-  _SplashState createState() => new _SplashState();
-}
-
-class _SplashState extends State<Splash> {
-  @override
-  void initState() {
-    super.initState();
-
-    new Future.delayed(const Duration(seconds: 3))
-        .then((value) => handleTimeout());
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: new Center(
-        child: const CircularProgressIndicator(),
-      ),
-    );
-  }
-
-  void handleTimeout() {
-    Navigator.of(context).pushReplacementNamed('/list');
-  }
-}
-
 class VideoList extends StatefulWidget {
   @override
   _VideoListState createState() => new _VideoListState();
 }
 
 class _VideoListState extends State<VideoList> {
+  static String word;
   @override
   void initState() {
     super.initState();
+    _init();
   }
 
-  void search(String text) {
+  void _init() async {}
+
+  Future<Map<String, dynamic>> _search(String word) async {
     // TODO search
+    return null; //TODO
+  }
+
+  List<Widget> _getListItems(Map<String, dynamic> data) {
+    return []; //TODO
   }
 
   @override
   Widget build(BuildContext context) {
-    var strings = Strings.of(context);
-    var title = strings == null ? 'main: title: null' : strings.title;
-    var apiKey = strings == null ? 'main: apiKey: null' : strings.apiKey;
-    if (title == null) title = 'main: title: null';
-    if (apiKey == null) apiKey = 'main: apiKey: null';
     return Scaffold(
       appBar: new AppBar(
-        title: Text(title),
+        title: Text(Strings.of(context).title),
       ),
-      body: new Center(
-        child: Text(apiKey),
+      body: FutureBuilder(
+        future: _search(word),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue),
+              ),
+            );
+          } else {
+            return ListView(children: _getListItems(snapshot.data));
+          }
+        },
       ),
     );
   }
@@ -144,5 +107,4 @@ class VideoPlayer extends StatelessWidget {
       ),
     );
   }
-
 }
